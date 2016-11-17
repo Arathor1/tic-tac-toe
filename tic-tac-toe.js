450d27015722b69d845af28084708a8a93db6a45
Arath Latorre <latorre.arathsek@sekquito.com>
2016-11-17 11:22:38 -0800

    finished

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,9 @@
 // Write all your JavaScript code in this file!
 // You can use the namespace variable that's defined here.
 var namespace = "http://www.w3.org/2000/svg"
+var turn = "player1"
 function leftuppercircle(){
+  if(turn == "player1"){
   var rightCircle = document.createElementNS(namespace, "circle")
   rightCircle.setAttribute("cx", 45)
   rightCircle.setAttribute("cy", 45)
@@ -9,8 +11,12 @@ function leftuppercircle(){
   rightCircle.setAttribute("fill", "yellow")
   var canvas = document.getElementById("game-board")
   canvas.appendChild(rightCircle)
-}
+  turn = "player2"
+}else{
+}}
+
  function middleuppercircle(){
+   if(turn == "player2"){
    var middleCircle = document.createElementNS(namespace, "circle")
    middleCircle.setAttribute("cx", 145)
    middleCircle.setAttribute("cy", 45)
@@ -18,8 +24,12 @@ function leftuppercircle(){
    middleCircle.setAttribute("fill", "green")
    var canvas = document.getElementById("game-board")
    canvas.appendChild(middleCircle)
- }
+   turn = "player1"
+ }else{
+ }}
+
 function rightuppercircle(){
+  if(turn == "player1"){
   var rightCircle = document.createElementNS(namespace, "circle")
   rightCircle.setAttribute("cx", 245)
   rightCircle.setAttribute("cy", 45)
@@ -27,8 +37,12 @@ function rightuppercircle(){
   rightCircle.setAttribute("fill", "yellow")
   var canvas = document.getElementById("game-board")
   canvas.appendChild(rightCircle)
-}
+  turn = "player2"
+}else{
+}}
+
 function leftmiddlecircle(){
+  if(turn == "player2"){
   var leftMiddleCircle = document.createElementNS(namespace, "circle")
   leftMiddleCircle.setAttribute("cx", 45)
   leftMiddleCircle.setAttribute("cy", 145)
@@ -36,8 +50,12 @@ function leftmiddlecircle(){
   leftMiddleCircle.setAttribute("fill", "green")
   var canvas = document.getElementById("game-board")
   canvas.appendChild(leftMiddleCircle)
-}
+  turn = "player1"
+}else{
+}}
+
 function middlemiddlecircle(){
+  if(turn == "player1"){
   var middleMiddleCircle = document.createElementNS(namespace, "circle")
   middleMiddleCircle.setAttribute("cx", 145)
   middleMiddleCircle.setAttribute("cy", 145)
@@ -45,8 +63,12 @@ function middlemiddlecircle(){
   middleMiddleCircle.setAttribute("fill", "yellow")
   var canvas = document.getElementById("game-board")
   canvas.appendChild(middleMiddleCircle)
-}
+  turn = "player2"
+}else{
+}}
+
 function rightmiddlecircle(){
+  if(turn == "player2"){
   var rightMiddleCircle = document.createElementNS(namespace, "circle")
   rightMiddleCircle.setAttribute("cx", 245)
   rightMiddleCircle.setAttribute("cy", 145)
@@ -54,8 +76,12 @@ function rightmiddlecircle(){
   rightMiddleCircle.setAttribute("fill", "green" )
   var canvas = document.getElementById("game-board")
   canvas.appendChild(rightMiddleCircle)
-}
+  turn = "player1"
+}else{
+}}
+
 function leftdowncircle(){
+  if(turn == "player1"){
   var leftDownCircle = document.createElementNS(namespace, "circle")
   leftDownCircle.setAttribute("cx", 45)
   leftDownCircle.setAttribute("cy", 245)
@@ -63,8 +89,12 @@ function leftdowncircle(){
   leftDownCircle.setAttribute("fill","yellow")
   var canvas = document.getElementById("game-board")
   canvas.appendChild(leftDownCircle)
-}
+  turn = "player2"
+}else{
+}}
+
 function middledowncircle(){
+  if(turn == "player2"){
   var middleDownCircle = document.createElementNS(namespace, "circle")
   middleDownCircle.setAttribute("cx", 145)
   middleDownCircle.setAttribute("cy", 245)
@@ -72,8 +102,12 @@ function middledowncircle(){
   middleDownCircle.setAttribute("fill", "green" )
   var canvas = document.getElementById("game-board")
   canvas.appendChild(middleDownCircle)
-}
+  turn = "player1"
+}else{
+}}
+
 function rightdowncircle(){
+  if(turn == "player1") {
   var rightDownCircle = document.createElementNS(namespace, "circle")
   rightDownCircle.setAttribute("cx", 245)
   rightDownCircle.setAttribute("cy", 245)
@@ -81,4 +115,6 @@ function rightdowncircle(){
   rightDownCircle.setAttribute("fill", "yellow" )
   var canvas = document.getElementById("game-board")
   canvas.appendChild(rightDownCircle)
+  turn = "player2"
+}
 }
